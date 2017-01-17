a8cba067e64f1a66f94b36e4325afb74e06747d5
Stefan B <Stefan.bie@live.de>
2017-01-17 11:50:50 +0100

    - changed default port to 8000

--- service-context/src/index.js
+++ service-context/src/index.js
@@ -1,6 +1,6 @@
 let Server = require("./Server").Server
 
-let router_url = process.env.WAMP_ROUTER_URL ? process.env.WAMP_ROUTER_URL :  "ws://localhost:8080/ws" ;
+let router_url = process.env.WAMP_ROUTER_URL ? process.env.WAMP_ROUTER_URL :  "ws://localhost:8000/ws" ;
 let router_realm = process.env.WAMP_RELAM ? process.env.WAMP_RELAM :  "realm1";
 
 //Create new server instance and start the server
