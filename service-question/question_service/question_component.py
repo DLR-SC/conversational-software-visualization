c5ea446c684f58dc06c1d92fb09f618c4cbc5b82
Stefan B <Stefan.bie@live.de>
2017-01-20 00:22:16 +0100

    - add support for pattern based receiving of messages

--- service-question/question_service/question_component.py
+++ service-question/question_service/question_component.py
@@ -1,13 +1,17 @@
 from autobahn.asyncio.wamp import ApplicationSession
-
+from autobahn.wamp.types import SubscribeOptions
+import functools
 
 class QuestionComponent(ApplicationSession):
     """
     An application component providing procedures with different kinds
     of arguments.
     """
-    def get_channel_from_event(self,event):
-        return event.topic
+    def get_channel_from_details(self,details):
+        arr = details.topic.split(".")
+        indexOfMessage = arr.index("channel")
+        return arr[indexOfMessage+1]
+
     def convert_to_intent(self, sentence):
 
         if sentence == "Wo ist Klasse xyz?":
@@ -18,17 +22,17 @@ class QuestionComponent(ApplicationSession):
                     }
 
 
+    def onIncommingMessage(self,message, details):
+        print("{0}".format(message))
+        channel_id = self.get_channel_from_details(details)
+
+        print("got message {0}".format(channel_id))
+        self.publish(u'sofia.channel.{0}.messages.OUTGOING_MESSAGE'.format(channel_id), {
+            "text": u'The force will be with you always',
+            "channel": channel_id})
+
+
     async def onJoin(self, details):
 
-        def got_message(incomming_msg,obj,event):
-            incoming_text = incomming_msg['text']
-            channel = self.get_channel_from_event(event)
-            print("incomming: {0}".format(incomming_msg))
-            self.publish(u'sofia.channel.{0}.messages.OUTGOING_MESSAGE'.format(channel), {
-                "text": u'The force will be with you always',
-                "channel": u'GENERAL'
-                                                              })
-
-
-        await self.subscribe(got_message, u'sofia.channel..messages.INCOMING_MESSAGE',{'match': 'wildcard'})
+        await self.subscribe(self.onIncommingMessage,"sofia.channel..messages.INCOMING_MESSAGE",options=SubscribeOptions(match='wildcard',details_arg='details'))
         print("Registered methods; ready for actions. Give me some... ")
