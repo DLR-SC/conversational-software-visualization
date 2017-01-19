ba6f3e26b9c6f8eb1b61afae11892afeb6b19d03
Stefan B <Stefan.bie@live.de>
2017-01-19 21:00:06 +0100

    changed to channel based topics

--- service-question/question_service/question_component.py
+++ service-question/question_service/question_component.py
@@ -6,7 +6,8 @@ class QuestionComponent(ApplicationSession):
     An application component providing procedures with different kinds
     of arguments.
     """
-
+    def get_channel_from_event(self,event):
+        return event.topic
     def convert_to_intent(self, sentence):
 
         if sentence == "Wo ist Klasse xyz?":
@@ -19,14 +20,15 @@ class QuestionComponent(ApplicationSession):
 
     async def onJoin(self, details):
 
-        def got_message(incomming_msg):
+        def got_message(incomming_msg,obj,event):
             incoming_text = incomming_msg['text']
+            channel = self.get_channel_from_event(event)
             print("incomming: {0}".format(incomming_msg))
-            self.publish(u'sofia.messages.OUTGOING_MESSAGE', {
+            self.publish(u'sofia.channel.{0}.messages.OUTGOING_MESSAGE'.format(channel), {
                 "text": u'The force will be with you always',
                 "channel": u'GENERAL'
                                                               })
 
 
-        await self.subscribe(got_message, u'sofia.messages.INCOMING_MESSAGE')
+        await self.subscribe(got_message, u'sofia.channel..messages.INCOMING_MESSAGE',{'match': 'wildecard'})
         print("Registered methods; ready for actions. Give me some... ")
