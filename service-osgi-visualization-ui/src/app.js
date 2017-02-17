db26a9f38d8e832d938d769eadd02ae2073ff4c0
Stefan B <Stefan.bie@live.de>
2017-02-17 18:45:29 +0100

    - add functions to react on namespace events

--- service-osgi-visualization-ui/src/app.js
+++ service-osgi-visualization-ui/src/app.js
@@ -9,17 +9,15 @@ class App {
   constructor(options){
 
     options = options ? options : {};
-    console.info("Created app")
     let osgiApiUrl = options.osgiApiUrl ? options.osgiApiUrl: "https://osgi-api.cloud.conts.de";
     let svgCanvas = options.svgCanvas  ? options.svgCanvas : d3.select("#bundles-svg");
 
-    this.initBundleVis(osgiApiUrl,svgCanvas);
+    this.initVisPromise = this.initBundleVis(osgiApiUrl,svgCanvas);
 
 
     let WAMP_ROUTER_URL = options.WAMP_ROUTER_URL ? options.WAMP_ROUTER_URL : "ws://cloud.conts.de:8090/ws";
     let WAMP_RELAM = options.WAMP_RELAM ? options.WAMP_RELAM : "realm1";
 
-      console.log("connectWamp")
     this.connectWamp(WAMP_ROUTER_URL,WAMP_RELAM)
 
   }
@@ -36,6 +34,7 @@ class App {
         .then((res)=>res.json());
 
     ImExportsGraphPromise.then((data)=>console.log(data));
+    window.displayNamespace = this.displayNamespace.bind(this);
 
     return Promise.all([bundlesPromise,ImExportsGraphPromise]).then((results)=>{
       let bundles = results[0];
@@ -43,20 +42,55 @@ class App {
 
       vis.renderBundleGraph(bundles, imExports, svgCanvas);
 
-      /*main.useMetric(metrics.getMetric("bundles-size-metric-uniform"));
+      main.useMetric(metrics.getMetric("bundles-size-metric-package-count"));
       main.useMetric(metrics.getMetric("bundles-color-metric-uniform"));
       main.useMetric(metrics.getMetric("dependencies-color-metric-uniform"));
-      main.useMetric(metrics.getMetric("dependencies-width-metric-uniform"));*/
-      return true
+      main.useMetric(metrics.getMetric("dependencies-width-metric-uniform"));
+      return [bundles,imExports]
     });
   }
   connectWamp(url, relam){
-     console.log(url,relam)
-    return utils.connectToServer(url,relam,this.onJoin).catch(()=>console.error(arguments))
+    return utils.connectToServer(url,relam,this.onJoin.bind(this)).catch(()=>console.error(arguments))
   }
 
-  onJoin(){
+  onNamespaceMessage(msgArr,obj,event){
+    const channelId = utils.getChannelIdFromEvent(event);
+    const msg = msgArr[0];
+    console.log(msg, channelId);
+
+    this.displayNamespace(msg.namespace)
+  }
+
+  displayNamespace(namespace){
+    this.initVisPromise.then((result)=>{
+      const allBundles = result[0];
+
+      const unmatchedBundlesSelection = vis.getD3Bundles((d3selection)=>{
+        console.log( d3selection.bundle.symbolicName, d3selection.bundle.symbolicName.indexOf(namespace) < 0);
+        return d3selection.bundle.symbolicName.indexOf(namespace) < 0;
+      });
+      vis.resetGrayedOutBundles();
+      vis.grayOutBundles(unmatchedBundlesSelection);
+
+      const lengthOfMatchedBundles = Math.abs(unmatchedBundlesSelection[0].length - allBundles.length);
+      console.log("unmatchedBundlesSelection",unmatchedBundlesSelection[0]);
+      console.log("lengthOfMatchedBundles",unmatchedBundlesSelection[0].length,allBundles.length);
+      console.log("lengthOfMatchedBundles",lengthOfMatchedBundles);
+      if(lengthOfMatchedBundles <= allBundles.length/2 ){
+        main.useMetric(metrics.getMetric("dependencies-color-metric-direction"));
+        main.useMetric(metrics.getMetric("dependencies-width-metric-uniform"));
+
+        vis.enableRequiredLinks();
+        vis.enableImportLinks();
+        vis.showLinksOn("always");
+      }
+    });
+  }
+  onJoin(session){
       console.log("onJoin")
+      session.subscribe("sofia.channel..messages.Namespace",this.onNamespaceMessage.bind(this),{ match: "wildcard" })
+          .then(()=>console.log("subscribed"))
+          .catch(()=>console.error("errror"));
   }
 
 }
